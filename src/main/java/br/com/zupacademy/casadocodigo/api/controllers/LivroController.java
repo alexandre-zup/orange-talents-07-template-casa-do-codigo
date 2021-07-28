55556134918537ecf3cec5611173a978375da1c8
alexandre-zup <alexandre.santos@zup.com.br>
2021-07-28 11:55:23 -0300

    Melhorando a coesão dos controllers

--- src/main/java/br/com/zupacademy/casadocodigo/api/controllers/LivroController.java
+++ src/main/java/br/com/zupacademy/casadocodigo/api/controllers/LivroController.java
@@ -1,18 +1,16 @@
 package br.com.zupacademy.casadocodigo.api.controllers;
 
-import br.com.zupacademy.casadocodigo.api.dto.request.NovoLivroRequest;
 import br.com.zupacademy.casadocodigo.api.dto.response.DetalheLivroResponse;
 import br.com.zupacademy.casadocodigo.api.dto.response.LivroResponse;
 import br.com.zupacademy.casadocodigo.model.entities.Livro;
-import br.com.zupacademy.casadocodigo.model.repositories.AutorRepository;
-import br.com.zupacademy.casadocodigo.model.repositories.CategoriaRepository;
 import br.com.zupacademy.casadocodigo.model.repositories.LivroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.transaction.Transactional;
-import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,17 +20,6 @@ public class LivroController {
 
     @Autowired
     private LivroRepository livroRepository;
-    @Autowired
-    private AutorRepository autorRepository;
-    @Autowired
-    private CategoriaRepository categoriaRepository;
-
-    @PostMapping
-    @Transactional
-    public void cadastra(@RequestBody @Valid NovoLivroRequest request) {
-        Livro livro = request.toModel(autorRepository, categoriaRepository);
-        livroRepository.save(livro);
-    }
 
     @GetMapping
     public List<LivroResponse> lista() {
---
+++ src/main/java/br/com/zupacademy/casadocodigo/api/controllers/NovoLivroController.java
@@ -0,0 +1,33 @@
+package br.com.zupacademy.casadocodigo.api.controllers;
+
+import br.com.zupacademy.casadocodigo.api.dto.request.NovoLivroRequest;
+import br.com.zupacademy.casadocodigo.model.entities.Livro;
+import br.com.zupacademy.casadocodigo.model.repositories.AutorRepository;
+import br.com.zupacademy.casadocodigo.model.repositories.CategoriaRepository;
+import br.com.zupacademy.casadocodigo.model.repositories.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/livros")
+public class NovoLivroController {
+    @Autowired
+    private LivroRepository livroRepository;
+    @Autowired
+    private AutorRepository autorRepository;
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @PostMapping
+    @Transactional
+    public void cadastra(@RequestBody @Valid NovoLivroRequest request) {
+        Livro livro = request.toModel(autorRepository, categoriaRepository);
+        livroRepository.save(livro);
+    }
+}
