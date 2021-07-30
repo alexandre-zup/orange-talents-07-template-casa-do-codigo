c8ec18b490425c987ad9112da6e98799f8a52da4
alexandre-zup <alexandre.santos@zup.com.br>
2021-07-30 11:59:27 -0300

    Melhorando o validador de CPF/CNPJ

--- src/main/java/br/com/zupacademy/casadocodigo/api/validation/BrazilianDocument.java
+++ src/main/java/br/com/zupacademy/casadocodigo/api/validation/BrazilianDocument.java
@@ -1,11 +1,21 @@
 package br.com.zupacademy.casadocodigo.api.validation;
 
+import org.hibernate.validator.constraints.CompositionType;
+import org.hibernate.validator.constraints.ConstraintComposition;
+import org.hibernate.validator.constraints.br.CNPJ;
+import org.hibernate.validator.constraints.br.CPF;
+
 import javax.validation.Constraint;
 import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
 import java.lang.annotation.*;
 
+@CPF
+@CNPJ
+@ConstraintComposition(CompositionType.OR)
+@ReportAsSingleViolation
 @Documented
-@Constraint(validatedBy = BrazilianDocumentValidator.class)
+@Constraint(validatedBy = { })
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface BrazilianDocument {
